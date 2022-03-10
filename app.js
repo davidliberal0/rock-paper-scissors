ec7bdeab96b934309167e72e6cfd1e3f481222e7
David Liberal <davidliberal0@gmail.com>
2022-03-09 19:43:51 -0500

    implement player choice input

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ function computerPlay() {
 
 // stores the randomized commputer's play selection
 let computerSelection = computerPlay();
+
+const playChoice = ["Rock", "Paper", "Scissors"];
+
+// Function for player
+function playerPlay() {
+  let play = null;
+
+  //   //   while (play != "rock" || play != "paper" || play != "scissors") {
+  //   //     let play = prompt(
+  //   //       "Please select a choice: Rock, Paper or Scissors.."
+  //   //     ).toLowerCase();
+  //   //   }
+
+  do {
+    play = prompt(
+      "Please select a choice: Rock, Paper or Scissors.."
+    ).toLowerCase();
+  } while (play != "rock" || play != "paper" || play != "scissors");
+
+  if (play === "rock") {
+    return playChoice[0];
+  } else if (play === "paper") {
+    return playChoice[1];
+  } else {
+    return playChoice[2];
+  }
+}
+
+let playerSelection = playerPlay();
