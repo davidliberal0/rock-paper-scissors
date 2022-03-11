330883c51c854dc9024b8a904167e10146abcf70
David Liberal <davidliberal0@gmail.com>
2022-03-11 13:55:43 -0500

    fix game logic

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+  if (playerSelection == computerSelection) {
     console.log("Tie");
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
     console.log("You win! Rock destroys scissors.");
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return (playerScore += 1);
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
     console.log("You win! Paper covers rock.");
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return (playerScore += 1);
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
     console.log("You win! Scissors cut paper.");
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return (playerScore += 1);
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
     console.log("You lose! Scissors cut paper.");
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return (computerScore += 1);
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
     console.log("You lose! Paper covers rock");
+    return (computerScore += 1);
   } else {
     console.log("You Lose! Rock destroys scissors.");
+    return (computerScore += 1);
   }
 }
 
@@ -23,13 +32,11 @@ const playChoice = ["Rock", "Paper", "Scissors"];
 // and will randomly return Rock, Paper, or Scissors
 function computerPlay() {
   // will return a random number string within the boundary of the index
-  return playChoice[
-    Math.floor(Math.random() * (playChoice.length - 1 - 0) + 0)
-  ];
+  return playChoice[Math.floor(Math.random() * (playChoice.length - 0) + 0)];
 }
 
 // stores the randomized commputer's play selection
-let computerSelection = computerPlay();
+// let computerSelection = computerPlay();
 
 // Function for player
 function playerPlay() {
@@ -46,4 +53,19 @@ function playerPlay() {
   }
 }
 
-let playerSelection = playerPlay();
+// let playerSelection = playerPlay();
+
+function game() {
+  // Controls the amount of rounds for the game
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = playerPlay().toLowerCase();
+    console.log(`Player: ${playerSelection}`);
+    let computerSelection = computerPlay().toLowerCase();
+    console.log(`Computer: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Round You:${playerScore} || Computer ${computerScore}`);
+    console.log("------------------------------------------------------");
+  }
+}
+
+game();
