f61d3163aa791acbbba7adefa39c320fd928cd83
David Liberal <davidliberal0@gmail.com>
2022-04-02 13:39:55 -0400

    fix broken logic and ui interaction

--- app.js
+++ app.js
@@ -1,6 +1,66 @@
 let playerScore = 0;
 let computerScore = 0;
 
+// Play Choices Array
+const playChoice = ["Rock", "Paper", "Scissors"];
+
+// Randomizes computer choice by randomly selecting a position in playChoice array
+function computerPlay() {
+  // will return a random number string within the boundary of the index
+  return playChoice[Math.floor(Math.random() * (playChoice.length - 0) + 0)];
+}
+
+// Selects all computer buttons
+const computerButtons = document.querySelectorAll(".computer-choice");
+
+// // Displays a shadow behind button of the computer's selected choice
+// function OnScrnComputerButton(computerSelection) {
+//   // let computerSelection = computerPlay().toLowerCase();
+
+//   for (let computerButton of computerButtons) {
+//     if (computerButton.innerText.toLowerCase() === computerSelection) {
+//       computerButton.style.backgroundColor = "#4e5e9e";
+//       computerButton.style.boxShadow =
+//         "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
+//     } else if (computerButton.innerText.toLowerCase() === computerSelection) {
+//       computerButton.style.backgroundColor = "#4e5e9e";
+//       computerButton.style.boxShadow =
+//         "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
+//     } else {
+//       computerButton.style.backgroundColor = "#4e5e9e";
+//       computerButton.style.boxShadow =
+//         "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
+//     }
+//   }
+// }
+
+// Select all human buttons
+const humanButtons = document.querySelectorAll(".button-selector");
+
+for (let humanButton of humanButtons) {
+  humanButton.addEventListener("click", () => {
+    if (humanButton.innerText.toLowerCase() === "rock") {
+      let playerSelection = humanButton.innerText.toLowerCase();
+      let computerSelection = computerPlay().toLowerCase();
+      playRound(playerSelection, computerSelection);
+      document.querySelector("#human-score").innerText = `${playerScore}`;
+      document.querySelector("#computer-score").innerText = `${computerScore}`;
+    } else if (humanButton.innerText.toLowerCase() === "paper") {
+      let playerSelection = humanButton.innerText.toLowerCase();
+      let computerSelection = computerPlay().toLowerCase();
+      playRound(playerSelection, computerSelection);
+      document.querySelector("#human-score").innerText = `${playerScore}`;
+      document.querySelector("#computer-score").innerText = `${computerScore}`;
+    } else {
+      let playerSelection = humanButton.innerText.toLowerCase();
+      let computerSelection = computerPlay().toLowerCase();
+      playRound(playerSelection, computerSelection);
+      document.querySelector("#human-score").innerText = `${playerScore}`;
+      document.querySelector("#computer-score").innerText = `${computerScore}`;
+    }
+  });
+}
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     console.log("Tie");
@@ -24,78 +84,3 @@ function playRound(playerSelection, computerSelection) {
     return (computerScore += 1);
   }
 }
-
-// Play Choices Array
-const playChoice = ["Rock", "Paper", "Scissors"];
-
-// This function will return the computer's play
-// and will randomly return Rock, Paper, or Scissors
-function computerPlay() {
-  // will return a random number string within the boundary of the index
-  return playChoice[Math.floor(Math.random() * (playChoice.length - 0) + 0)];
-}
-
-// stores the randomized commputer's play selection
-// let computerSelection = computerPlay();
-
-// ----------------------------------------------
-// Select all human buttons
-const humanButtons = document.querySelectorAll(".button-selector");
-
-// Function for player
-function playerPlay() {
-  // Gives all human buttons functions
-  for (let humanButton of humanButtons) {
-    humanButton.addEventListener("click", () => {
-      if (humanButton.innerText === "rock") {
-        return playChoice[0];
-      } else if (humanButton.innerText === "paper") {
-        return playChoice[1];
-      } else {
-        return playChoice[2];
-      }
-    });
-  }
-}
-
-// -------------------------------------
-
-const computerButtons = document.querySelectorAll(".computer-choice");
-
-function OnScrnComputerButton() {
-  let computerSelection = computerPlay().toLowerCase();
-
-  for (let computerButton of computerButtons) {
-    if (computerButton.innerText === computerSelection) {
-      computerButton.style.backgroundColor = "#4e5e9e";
-      computerButton.style.boxShadow =
-        "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
-    } else if (computerButton.innerText === computerSelection) {
-      computerButton.style.backgroundColor = "#4e5e9e";
-      computerButton.style.boxShadow =
-        "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
-    } else {
-      computerButton.style.backgroundColor = "#4e5e9e";
-      computerButton.style.boxShadow =
-        "rgba(rgba(255, 225, 255, 0.56) 0px 22px 70px 4px";
-    }
-  }
-}
-
-// --------------------------------
-
-// let playerSelection = playerPlay();
-
-function game() {
-  // Controls the amount of rounds for the game
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay();
-    OnScrnComputerButton();
-    playRound(playerSelection, computerSelection);
-    document.querySelector("#computer-score").innerText = `${computerScore}`;
-    document.querySelector("#human-score").innerText = `${playerScore}`;
-  }
-}
-
-game();
