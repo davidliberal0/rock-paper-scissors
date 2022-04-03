d9321bd9d034c7cd39d2f4bca0f2de8b1ccd24f6
David Liberal <davidliberal0@gmail.com>
2022-04-02 21:09:26 -0400

    add play game function and display winner message

--- app.js
+++ app.js
@@ -36,51 +36,67 @@ const computerButtons = document.querySelectorAll(".computer-choice");
 
 // Select all human buttons
 const humanButtons = document.querySelectorAll(".button-selector");
+const winMessage = document.querySelector("#winning-message");
 
-for (let humanButton of humanButtons) {
-  humanButton.addEventListener("click", () => {
-    if (humanButton.innerText.toLowerCase() === "rock") {
-      let playerSelection = humanButton.innerText.toLowerCase();
-      let computerSelection = computerPlay().toLowerCase();
-      playRound(playerSelection, computerSelection);
-      document.querySelector("#human-score").innerText = `${playerScore}`;
-      document.querySelector("#computer-score").innerText = `${computerScore}`;
-    } else if (humanButton.innerText.toLowerCase() === "paper") {
-      let playerSelection = humanButton.innerText.toLowerCase();
-      let computerSelection = computerPlay().toLowerCase();
-      playRound(playerSelection, computerSelection);
-      document.querySelector("#human-score").innerText = `${playerScore}`;
-      document.querySelector("#computer-score").innerText = `${computerScore}`;
-    } else {
-      let playerSelection = humanButton.innerText.toLowerCase();
-      let computerSelection = computerPlay().toLowerCase();
-      playRound(playerSelection, computerSelection);
-      document.querySelector("#human-score").innerText = `${playerScore}`;
-      document.querySelector("#computer-score").innerText = `${computerScore}`;
-    }
-  });
+function playGame() {
+  for (let humanButton of humanButtons) {
+    humanButton.addEventListener("click", () => {
+      if (humanButton.innerText.toLowerCase() === "rock") {
+        let playerSelection = humanButton.innerText.toLowerCase();
+        let computerSelection = computerPlay().toLowerCase();
+        playRound(playerSelection, computerSelection);
+        displayScores();
+      } else if (humanButton.innerText.toLowerCase() === "paper") {
+        let playerSelection = humanButton.innerText.toLowerCase();
+        let computerSelection = computerPlay().toLowerCase();
+        playRound(playerSelection, computerSelection);
+        displayScores();
+      } else {
+        let playerSelection = humanButton.innerText.toLowerCase();
+        let computerSelection = computerPlay().toLowerCase();
+        playRound(playerSelection, computerSelection);
+        displayScores();
+      }
+    });
+  }
+
+  if (playerScore == 5) {
+    winMessage.innerText = innerText = "You Win";
+  } else {
+    winMessage.innerText = "Computer Wins";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    console.log("Tie");
+    winMessage.innerText = "Tie";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("You win! Rock destroys scissors.");
+    winMessage.innerText = "You win! Rock destroys scissors.";
     return (playerScore += 1);
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("You win! Paper covers rock.");
+    winMessage.innerText = "You win! Paper covers rock.";
     return (playerScore += 1);
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("You win! Scissors cut paper.");
+    winMessage.innerText = "You win! Scissors cut paper.";
     return (playerScore += 1);
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("You lose! Scissors cut paper.");
+    winMessage.innerText = "You lose! Scissors cut paper.";
     return (computerScore += 1);
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("You lose! Paper covers rock");
+    winMessage.innerText = "You lose! Paper covers rock";
     return (computerScore += 1);
   } else {
-    console.log("You Lose! Rock destroys scissors.");
+    winMessage.innerText = "You Lose! Rock destroys scissors.";
     return (computerScore += 1);
   }
 }
+
+const humanScore = document.querySelector("#human-score");
+const cpuScore = document.querySelector("#computer-score");
+
+function displayScores() {
+  humanScore.innerText = `${playerScore}`;
+  cpuScore.innerText = `${computerScore}`;
+}
+
+playGame();
